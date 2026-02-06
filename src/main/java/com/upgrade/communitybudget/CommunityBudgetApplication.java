1975f73d3f8bbe6009cbe3576c3685c9f2de2fe1
Carrillo1992 <danielcarrillo1992@gmail.com>
2026-02-06 22:19:13 +0100

    feat(init): initialize communitybudget spring boot project
    Add initial project setup with the following:
    - Spring Boot application structure.
    - OpenAPI documentation configuration.
    - Security configuration with basic settings.
    - Maven wrapper and `pom.xml` with necessary dependencies.
    - `.gitignore` file to exclude IDE and build-related files.
    - `application.properties` for environment variable initialization.
    - OpenAPI YAML specification for API endpoints.
    
    This sets up the foundation for the CommunityBudget project.

---
+++ src/main/java/com/upgrade/communitybudget/CommunityBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.upgrade.communitybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityBudgetApplication.class, args);
+    }
+
+}
